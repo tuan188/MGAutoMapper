53dc630e3e2d094619a98406ca77d6f9b002a721
Truong Anh Tuan <tuan188@gmail.com>
2015-12-18 14:51:31 +0700

    Add project

---
+++ MGAutoMapper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MGAutoMapper
+//
+//  Created by Tuan Truong on 12/18/15.
+//  Copyright © 2015 Tuan Truong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
